72f08a9823b45dd12ed3b2b5fb9707f19665cb87
NguyenTheVinh <nvinh1602@gmail.com>
2026-04-17 14:00:16 +0700

    refactor: remove unused Employee and Position code

--- FresherMisa2026/FresherMisa2026.Application/Services/Department/DepartmentService.cs
+++ FresherMisa2026/FresherMisa2026.Application/Services/Department/DepartmentService.cs
@@ -1,11 +1,7 @@
 ﻿using FresherMisa2026.Application.Interfaces;
 using FresherMisa2026.Application.Interfaces.Repositories;
 using FresherMisa2026.Application.Interfaces.Services;
-using FresherMisa2026.Entities;
 using FresherMisa2026.Entities.Department;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FresherMisa2026.Application.Services
 {
