1523242dad2d94638d085190f5e87e63cd9e3c41
Ngo Tien Dung <dungyoshino2004@gmail.com>
2026-04-18 05:40:01 +0700

    feat(task-2.1): add employee validation

--- FresherMisa2026/FresherMisa2026.Application/Services/Employee/EmployeeService.cs
+++ FresherMisa2026/FresherMisa2026.Application/Services/Employee/EmployeeService.cs
@@ -1,10 +1,11 @@
-using FresherMisa2026.Application.Interfaces;
+﻿using FresherMisa2026.Application.Interfaces;
 using FresherMisa2026.Application.Interfaces.Repositories;
 using FresherMisa2026.Application.Interfaces.Services;
 using FresherMisa2026.Entities;
 using FresherMisa2026.Entities.Employee;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace FresherMisa2026.Application.Services
 {
@@ -45,12 +46,39 @@ namespace FresherMisa2026.Application.Services
 
             if (!string.IsNullOrEmpty(employee.EmployeeCode) && employee.EmployeeCode.Length > 20)
             {
-                errors.Add(new ValidationError("EmployeeCode", "Mã nhân viên không được vượt quá 20 ký tự"));
+                errors.Add(new ValidationError("EmployeeCode", "M\u00E3 nh\u00E2n vi\u00EAn kh\u00F4ng \u0111\u01B0\u1EE3c v\u01B0\u1EE3t qu\u00E1 20 k\u00FD t\u1EF1"));
             }
 
             if (string.IsNullOrEmpty(employee.EmployeeName))
             {
-                errors.Add(new ValidationError("EmployeeName", "Tên nhân viên không được để trống"));
+                errors.Add(new ValidationError("EmployeeName", "T\u00EAn nh\u00E2n vi\u00EAn kh\u00F4ng \u0111\u01B0\u1EE3c \u0111\u1EC3 tr\u1ED1ng"));
+            }
+
+            if (employee.DepartmentID == Guid.Empty)
+            {
+                errors.Add(new ValidationError("DepartmentID", "Ph\u00F2ng ban b\u1EAFt bu\u1ED9c ch\u1ECDn"));
+            }
+
+            if (employee.PositionID == Guid.Empty)
+            {
+                errors.Add(new ValidationError("PositionID", "V\u1ECB tr\u00ED b\u1EAFt bu\u1ED9c ch\u1ECDn"));
+            }
+
+            if (!string.IsNullOrEmpty(employee.Email) &&
+                !Regex.IsMatch(employee.Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                errors.Add(new ValidationError("Email", "Email không đúng định dạng"));
+            }
+
+            if (!string.IsNullOrEmpty(employee.PhoneNumber) &&
+                !Regex.IsMatch(employee.PhoneNumber, @"^(0[3|5|7|8|9])+([0-9]{8})$"))
+            {
+                errors.Add(new ValidationError("PhoneNumber", "Số điện thoại không đúng định dạng"));
+            }
+
+            if (employee.DateOfBirth.HasValue && employee.DateOfBirth.Value.Date >= DateTime.Today)
+            {
+                errors.Add(new ValidationError("DateOfBirth", "Ngày sinh phải nhỏ hơn ngày hiện tại"));
             }
 
             return errors;
